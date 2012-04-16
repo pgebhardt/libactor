99ad19cf08e4c06e13890d09a47f47a646348c22
schansge <grosser.knuff@googlemail.com>
2012-04-16 21:23:13 +0200

    processes are now spawned asynchonously using gcd

--- src/process.c
+++ src/process.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <dispatch/dispatch.h>
 #include "process.h"
 
 // spawn new process
 process_id process_spawn(process_process_function function) {
     // TODO
+    // get concurrent queue
+    dispatch_queue_t concurrentQueue = dispatch_get_global_queue(
+        DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+
     // call process function
-    function();
+    dispatch_async(concurrentQueue, ^(void) {
+            function();
+        });
 
     // return error pid
     return 0;
