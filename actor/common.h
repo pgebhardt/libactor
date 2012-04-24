13341516b40acf7398baaaca9f2e3291f5a3e690
schansge <grosser.knuff@googlemail.com>
2012-04-24 20:35:23 +0200

    standard type definitions moved to common.h

---
+++ actor/common.h
@@ -0,0 +1,18 @@
+#ifndef ACTOR_COMMON_H
+#define ACTOR_COMMON_H
+
+// size
+typedef unsigned int actor_size_t;
+
+// time
+typedef double actor_time_t;
+
+// node id
+typedef int actor_node_id_t;
+
+// process id
+typedef int actor_process_id_t;
+
+#define ACTOR_INVALID_ID (-1)
+
+#endif
