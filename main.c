e3e5d760b238165f4fcd6590675f37d6313551f5
schansge <grosser.knuff@googlemail.com>
2012-04-24 13:44:39 +0200

    supervisor example

--- main.c
+++ main.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
 #include "actor/actor.h"
-#include "actor/distributer.h"
 
-void main_process(actor_process_t self) {
-    // 
+actor_error_t main_process(actor_process_t main) {
+    // start dummy process
+    actor_process_id_t pid = actor_process_spawn(main->node, ^(actor_process_t self) {
+            // link to main
+            actor_process_link(self, main->node->nid, main->pid);
+
+            // die with error
+            return ACTOR_FAILURE;
+        });
+
+    // receive message
+    actor_message_t message = actor_message_receive(main, 10.0);
+
+    // check success
+    if (message == NULL) {
+        return ACTOR_FAILURE;
+    }
+
+    // cast message to error message
+    actor_process_error_message_t error_message = (actor_process_error_message_t)message->data;
+
+    // print error
+    printf("%d.%d died with %d!\n", error_message->nid, error_message->pid, error_message->error);
+
+    // release message
+    actor_message_release(message);
+
+    return ACTOR_SUCCESS;
 }
 
 int main(int argc, char* argv[]) {
     // create node
-    actor_node_t node = actor_node_create(0, 2000);
+    actor_node_t node = actor_node_create(0, 1000);
 
-    // start main process
-    actor_process_spawn(node, ^(actor_process_t self) {
-                main_process(self);
-            });
+    // spawn main process
+    actor_process_spawn(node, ^actor_error_t(actor_process_t self) {
+            return main_process(self);
+        });
 
     // release node
     actor_node_release(node);
