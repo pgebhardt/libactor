dbc0693fe89c5489883f8b32206a085354940897
schansge <grosser.knuff@googlemail.com>
2012-04-24 20:35:38 +0200

    error definitions moved to error.h

---
+++ actor/error.h
@@ -0,0 +1,19 @@
+#ifndef ACTOR_ERROR_H
+#define ACTOR_ERROR_H
+
+// error type
+typedef unsigned int actor_error_t;
+
+// standard errors
+#define ACTOR_SUCCESS                   (0)
+#define ACTOR_ERROR                     (1)
+#define ACTOR_ERROR_INVALUE             (2)
+#define ACTOR_ERROR_MEMORY              (3)
+#define ACTOR_ERROR_TIMEOUT             (4)
+#define ACTOR_ERROR_DISPATCH            (5)
+#define ACTOR_ERROR_NODEID              (6)
+#define ACTOR_ERROR_PROCESSID           (7)
+#define ACTOR_ERROR_TOO_MANY_PROCESSES  (8)
+#define ACTOR_ERROR_NETWORK             (9)
+
+#endif
