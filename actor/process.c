268eef9598784aa0ee93f26cf01ca43c6b34a5e0
schansge <grosser.knuff@googlemail.com>
2012-04-24 13:43:58 +0200

    link and unlink functions added

--- actor/process.c
+++ actor/process.c
@@ -20,6 +20,8 @@ actor_process_t actor_process_create(actor_node_t node) {
     process->message_queue = NULL;
     process->sleep_semaphore = NULL;
     process->node = node;
+    process->supervisor_nid = -1;
+    process->supervisor_pid = -1;
 
     // create sleep semaphore
     process->sleep_semaphore = dispatch_semaphore_create(0);
@@ -46,10 +48,10 @@ actor_process_t actor_process_create(actor_node_t node) {
     return process;
 }
 
-void actor_process_release(actor_process_t process) {
+actor_error_t actor_process_release(actor_process_t process) {
     // check for valid process
     if (process == NULL) {
-        return;
+        return ACTOR_FAILURE;
     }
 
     // release sleep semaphore
@@ -64,18 +66,55 @@ void actor_process_release(actor_process_t process) {
 
     // free process memory
     free(process);
+
+    return ACTOR_SUCCESS;
 }
 
 // sleep
-void actor_process_sleep(actor_process_t process, double time) {
+actor_error_t actor_process_sleep(actor_process_t process, actor_time_t time) {
     // check for correct input
     if ((process == NULL) || (time < 0.0)) {
-        return;
+        return ACTOR_FAILURE;
     }
 
     // wait for timeout
     dispatch_semaphore_wait(process->sleep_semaphore,
         dispatch_time(DISPATCH_TIME_NOW,
             (dispatch_time_t)(time * (double)NSEC_PER_SEC)));
+
+    return ACTOR_SUCCESS;
 }
 
+// link
+actor_error_t actor_process_link(actor_process_t process, actor_node_id_t supervisor_nid,
+    actor_process_id_t supervisor_pid) {
+    // check valid process
+    if (process == NULL) {
+        return ACTOR_FAILURE;
+    }
+
+    // check supervisor id
+    if ((supervisor_pid < 0) || (supervisor_nid < 0) ||
+        ((supervisor_nid == process->node->nid) && (supervisor_pid == process->pid))) {
+        return ACTOR_FAILURE;
+    }
+
+    // set supervisor
+    process->supervisor_nid = supervisor_nid;
+    process->supervisor_pid = supervisor_pid;
+
+    return ACTOR_SUCCESS;
+}
+
+actor_error_t actor_process_unlink(actor_process_t process) {
+    // check valid process
+    if (process == NULL) {
+        return ACTOR_FAILURE;
+    }
+
+    // unlink
+    process->supervisor_nid = -1;
+    process->supervisor_pid = -1;
+
+    return ACTOR_SUCCESS;
+}
--- actor/process.h
+++ actor/process.h
@@ -7,19 +7,36 @@ typedef struct {
     actor_message_queue_t message_queue;
     dispatch_semaphore_t sleep_semaphore;
     actor_node_t node;
+    actor_node_id_t supervisor_nid;
+    actor_process_id_t supervisor_pid;
 } actor_process_struct;
 typedef actor_process_struct* actor_process_t;
 
+// process error message
+typedef struct {
+    actor_node_id_t nid;
+    actor_process_id_t pid;
+    actor_error_t error;
+} actor_process_error_message_struct;
+typedef actor_process_error_message_struct* actor_process_error_message_t;
+
 // Process block signature
-typedef void (^actor_process_function_t)(actor_process_t const self);
+typedef actor_error_t (^actor_process_function_t)(actor_process_t const self);
 
 // create process
 actor_process_t actor_process_create(actor_node_t node);
 
 // release process
-void actor_process_release(actor_process_t process);
+actor_error_t actor_process_release(actor_process_t process);
 
 // sleep
-void actor_process_sleep(actor_process_t process, double time);
+actor_error_t actor_process_sleep(actor_process_t process, actor_time_t time);
+
+// link
+actor_error_t actor_process_link(actor_process_t process, actor_node_id_t supervisor_nid,
+    actor_process_id_t supervisor_pid);
+
+// unlink
+actor_error_t actor_process_unlink(actor_process_t process);
 
 #endif
