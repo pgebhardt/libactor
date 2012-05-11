1f705d47e3df72c3191d7853cacff4d1c5482dd3
schansge <grosser.knuff@googlemail.com>
2012-05-11 16:43:06 +0200

    License added

--- src/actor.c
+++ src/actor.c
@@ -1,6 +1,23 @@
-#include "actor.h"
+// libactor
+//
+// Implementation of an erlang style actor model using libdispatch
+// Copyright (C) 2012  Patrik Gebhardt
+// Contact: patrik.gebhardt@rub.de
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// interface functions
+#include "actor.h"
 
 // spawn process
 actor_error_t actor_spawn(actor_node_t node, actor_process_id_t* pid,
